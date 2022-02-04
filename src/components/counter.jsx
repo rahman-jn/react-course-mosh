16af0f829aa3db8c40f2d35539616f83e07b197b
git <git@git-client.com>
2022-02-04 18:50:15 +0330

    1 - We bind the methods by using arrow functions instead of defining them in cunstructor.
    Updating the states was implemented through setState method.

---
+++ src/components/counter.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Counter extends React.Component {
+    state = {
+        count : 1,
+        tags : ['tag1', 'tag2', 'tag3']
+        };
+
+    
+
+    renderTags(){
+        return this.state.tags.map(tag => <li key = { tag }>{ tag } </li>)
+    };
+
+    handleIncrement = () => {
+        this.setState({ count : this.state.count +1 } );
+    }
+
+    render() { 
+
+        return (
+        <div>
+            <span className={this.badgeClasses()}>{ this.formatCount() }</span>    
+            <button onClick = { this.handleIncrement } >Increment</button>
+        <div>
+            {this.state.tags.length === 0 && "There are no tags found!"}
+            { this.renderTags() }</div>
+        </div>
+        );
+    }
+
+    badgeClasses() {
+        let classes = "badge badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        console.log(classes);
+        return classes;
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
+}
+ 
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './components/counter'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Counter />,
   document.getElementById('root')
 );
 
