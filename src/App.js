a4d832e45449fdbcbcd54ab7e22e1830cf10de0e
git <git@git-client.com>
2022-02-05 16:42:24 +0330

    Previous commit was changed and both navbar and counters components  went undr the App component. Also the total number of rows shown on top in navbar component and updated if one of them deleted.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
-import Vidly from './components/vidly';
 import Navbar from './components/navbar';
+import Counters from './components/counters';
 
-function App() {
-  return (
-    <main className="container">
-      <Navbar />
-    </main>
-  );
+class App extends React.Component {
+  state = {
+    counters: [
+    {id : 1, value :2},
+    {id : 2, value :14},
+    {id : 3, value :5},
+    {id : 4, value :3}
+    ]
+};
+
+handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf( counter );
+    counters[index] = {...counter };
+    counters[index].value++;
+    this.setState({counters} );
 }
 
+handleDelete = (countId) =>{
+    const counters = this.state.counters.filter(c => c.id !== countId);
+    this.setState({ counters })
+}
+
+handleReset = () =>{
+     const counters = [...this.state.counters];
+     counters.map( c => c.value = 0 );
+     this.setState({ counters });
+}
+
+  render() { 
+    return (
+      <React.Fragment>
+        <Navbar 
+          totalCounts = { this.state.counters.map(c => c.value >0).length}
+         />
+        <Counters 
+                    onDelete = {this.handleDelete}
+                    onReset = {this.handleReset}
+                    onIncrement = {this.handleIncrement}
+                    counters = {this.state.counters}
+                />
+      </React.Fragment>
+    );
+  }
+}
+ 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,33 +3,7 @@ import Counters from './counters';
 
 class Navbar extends React.Component {
 
-    state = {
-        counters: [
-        {id : 1, value :2},
-        {id : 2, value :14},
-        {id : 3, value :5},
-        {id : 4, value :3}
-        ]
-    };
 
-    handleIncrement = (counter) => {
-        const counters = [...this.state.counters];
-        const index = counters.indexOf( counter );
-        counters[index] = {...counter };
-        counters[index].value++;
-        this.setState({counters} );
-    }
-
-    handleDelete = (countId) =>{
-        const counters = this.state.counters.filter(c => c.id !== countId);
-        this.setState({ counters })
-    }
-
-    handleReset = () =>{
-         const counters = [...this.state.counters];
-         counters.map( c => c.value = 0 );
-         this.setState({ counters });
-    }
 
 
     render() { 
@@ -37,13 +11,11 @@ class Navbar extends React.Component {
             <nav className="navbar navbar-default navbar-fixed-bottom">
                 <div className="container">
                     Navbar
+                    <span>
+                        {this.props.totalCounts}
+                    </span>
                 </div>
-                <Counters 
-                    onDelete = {this.handleDelete}
-                    onReset = {this.handleReset}
-                    onIncrement = {this.handleIncrement}
-                    counters = {this.state.counters}
-                />
+
             </nav>
         );
     }
