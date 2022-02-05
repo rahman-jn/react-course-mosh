14e2e0f463eb29c920921dfd1c7fac35a425b2c9
git <git@git-client.com>
2022-02-05 15:21:04 +0330

    Reset button added to project, This button set the value of all counters state to 0.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -24,10 +24,24 @@ class Counters extends React.Component {
         this.setState({ counters })
     }
 
+    handleReset = () =>{
+         const counters = [...this.state.counters];
+         counters.map( c => c.value = 0 );
+         this.setState({ counters });
+    }
+
     render() { 
         return <div>
-                   {this.state.counters.map(counter => <Counter key={counter.id} counter = {counter} 
-                   onDelete = {this.handleDelete} onIncrement = { () => this.handleIncrement(counter) } />)} 
+                    <button className="btn-primary btn-sm m-2" onClick={this.handleReset}>
+                        Reset
+                    </button>
+                   {this.state.counters.map(counter =>
+                   <Counter 
+                   key={counter.id}
+                   counter = {counter} 
+                   onDelete = {this.handleDelete}
+                   onIncrement = { () => this.handleIncrement(counter) }
+                    />)} 
                 </div>;
     }
 }
