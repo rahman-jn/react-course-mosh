9fa8b4313f40503f945906be25e04665c0edca96
git <git@git-client.com>
2022-02-05 17:05:50 +0330

    Navbar component converted to stateless functional component because  it doesn't have ant independent state.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,24 +1,18 @@
 import React, { Component } from 'react'
 import Counters from './counters';
 
-class Navbar extends React.Component {
-
-
-
-
-    render() { 
-        return (
-            <nav className="navbar navbar-default navbar-fixed-bottom">
+export const  Navbar = (props) => {
+    return ( 
+          <nav className="navbar navbar-default navbar-fixed-bottom">
                 <div className="container">
                     Navbar
                     <span>
-                        {this.props.totalCounts}
+                        {props.totalCounts}
                     </span>
                 </div>
 
             </nav>
-        );
-    }
+     );
 }
  
 export default Navbar;
