1a7a8a7267de4c763aa1550f11c7827338063929
git <git@git-client.com>
2022-02-06 16:52:25 +0330

    Like component converted to a stateless component and implemented by function instead of class.

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons'
 
-
-class Like extends React.Component {
-    render() { 
+const Like = (props) => { 
         let classes = "fa fa-heart";
-        if(!this.props.liked) classes += "-o" ;
+        if(!props.liked) classes += "-o" ;
         return (
-            <i style={{ cursor : "pointer" }} className={classes} onClick={ this.props.onClick } />
+            <i style={{ cursor : "pointer" }} className={classes} onClick={ props.onClick } />
         );
-    }
 }
  
 export default Like;
